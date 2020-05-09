2fa794fc4609d369178980470c94a24b9593ff03
ayush verma <vayush198@gmail.com>
2020-05-09 10:38:41 +0530

    Add:Added UC-11,ability to manage Employee Wage of multiple companies

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,25 +2,34 @@ public class EmpWageBuilder{
 	public static final int IS_FULL_TIME = 2;		//made final for constants
 	public static final int IS_PART_TIME = 1;
 
-	private final String company;
-	private final int workingDays;
-	private final int empRatePerHour;
-	private final int maxHourInMonth;
+	private int number_of_companies=0;
+	public CompanyEmpWage [] array;
 
-	public EmpWageBuilder(String company, int empRatePerHour,int workingDays, int maxHourInMonth) {
-		this.company=company;
-		this.workingDays=workingDays;
-		this.empRatePerHour=empRatePerHour;
-		this.maxHourInMonth=maxHourInMonth;
+	public EmpWageBuilder() {
+		array = new CompanyEmpWage[5];
 	}
 
-	public int calculate_empWage() {
+	//adding each company wage data
+	private void addCompanyWage(String company, int empRatePerHour, int empWorkingDay, int maxHourInMonth) {
+		array[number_of_companies] = new CompanyEmpWage(company,empRatePerHour,empWorkingDay,maxHourInMonth);
+		number_of_companies++;
+	}
+
+	//computing every company employee wage
+	private void computeEmployeeWage(){
+		for(int c=0; c<number_of_companies; c++) {
+			int totalEmployeeWage = this.calculate_empWage(array[c]);
+			System.out.println("Total Employee Wage for company "+array[c].company+" is "+totalEmployeeWage);
+		}
+	}
+
+	public int calculate_empWage(CompanyEmpWage companyEmpWage) {
 
 		int empHours = 0;
 		int empWorkingDay =0;
 		int totalEmpHours=0;
 
-		while(totalEmpHours <= maxHourInMonth && empWorkingDay < workingDays) {
+		while(totalEmpHours <= companyEmpWage.maxHourInMonth && empWorkingDay < companyEmpWage.workingDays) {
 			empWorkingDay++;
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch(empCheck) {
@@ -37,15 +46,29 @@ public class EmpWageBuilder{
 			totalEmpHours = totalEmpHours + empHours;
 			System.out.println("Day : "+ empWorkingDay+ " Employee Hour : "+empHours);
 		}
-		return totalEmpHours * empRatePerHour;
+		return totalEmpHours *companyEmpWage.empRatePerHour;
 	}
 
 	public static void main(String [] args) {
-		EmpWageBuilder dMart = new EmpWageBuilder("DMart",10,2,5);
-		EmpWageBuilder vMart = new EmpWageBuilder("VMart",20,4,25);
-
-		System.out.println("Total Employee Wage for company "+ dMart.company+" is : " +dMart.calculate_empWage());
-		System.out.println("Total Employee Wage for company "+ vMart.company+" is : "+vMart.calculate_empWage());
+		EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+		empWageBuilder.addCompanyWage("DMart",10,2,5);
+		empWageBuilder.addCompanyWage("VMart",20,4,25);
+		empWageBuilder.computeEmployeeWage();
 	}
 
+	public class CompanyEmpWage {
+
+		public final String company;
+		public final int empRatePerHour;
+		public final int workingDays;
+		public final int maxHourInMonth;
+
+		public CompanyEmpWage(String company, int empRatePerHour, int workingDays, int maxHourInMonth) {
+			this. company=company;
+                	this.empRatePerHour=empRatePerHour;
+                	this.workingDays=workingDays;
+                	this. maxHourInMonth=maxHourInMonth;
+		}
+
+	}
 }
